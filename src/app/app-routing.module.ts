1c8c52322b5b6fd93f36e8fff84ad9e8ec156630
Avihai Cohen <avihai.android@gmail.com>
2018-07-06 11:31:34 +0300

    Add github page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {GithubComponent} from './components/github/github.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'github', component: GithubComponent},
+	{ path: '', redirectTo: '/github', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
